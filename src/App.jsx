8bc06933c73b94b7defeb061b5d349acd967bc90
Ruan Oliveira <ruanwesley93@gmail.com>
2024-10-03 02:17:11 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './App.css'
+
+
+function App() {
+
+  return (
+    
+    <div className='caixaBranca'>
+      
+      <div className='astro'>
+        <img id='imgAstro' src="./image.png" alt="" />
+      </div>
+      <div className='texto'>
+        <h1>Bem-vindo!</h1><br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, et debitis?
+          Accusantium eveniet, deleniti ex exercitationem quae perferendis fuga expedita
+          vitae repellendus necessitatibus ullam modi quia, eum excepturi harum magni.</p>
+          <br />
+        <a id='botao' href="#">Read more</a>
+      </div>
+    </div>
+  )
+  }
+export default App
